b1b58e22268724f65e0b9472713e17051302296b
DarcMattz <jcarvajal_21UR0148@psu.edu.ph>
2024-02-08 20:39:38 +0800

    first commit

---
+++ Exercise2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int x = listBox1.SelectedIndices.Count - 1; x >= 0; x--)
+            {
+                int idx = listBox1.SelectedIndices[x];
+                listBox2.Items.Add(listBox1.Items[idx]);
+                listBox1.Items.RemoveAt(idx);
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        { 
+            for (int x = listBox2.SelectedIndices.Count - 1; x >= 0; x--)
+            {
+                int idx = listBox2.SelectedIndices[x];
+                listBox2.Items.RemoveAt(idx);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
